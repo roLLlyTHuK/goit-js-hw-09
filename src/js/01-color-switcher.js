ce6ad0c71c0a4b111b0626f27f806af5b0d485de
roLLlyTHuK <ircvipua@gmail.com>
2023-08-28 11:04:39 +0300

    +task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
 
+const startButton = document.querySelector(`[data-start]`);
+const stopButton = document.querySelector(`[data-stop]`);
+const body = document.body;
+let timer = null;
+
+startButton.disabled = false;
+stopButton.disabled = true;
+//! вмикаємо зміну кольору
+startButton.addEventListener('click', ()=> {
+    timer = setInterval(() => { 
+        const randomColor = getRandomHexColor();
+        body.style.backgroundColor = randomColor;
+        startButton.disabled = true;
+        stopButton.disabled = false;
+    }, 1000);
+});
+//! вимикаємо зміну кольору
+stopButton.addEventListener('click', () => { 
+    clearInterval(timer);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+});
